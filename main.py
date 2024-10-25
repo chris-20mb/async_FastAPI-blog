d8ccddc227ed9d6957d7c7fe4116ca689f35876d
Christopher <christophermachado222@gmail.com>
2024-10-24 21:23:10 -0300

    "rotas e endpoints com path parameter"

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from datetime import UTC, datetime
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"message": "Hello World!!!"}
+@app.get('/posts/{framework}')
+def read_posts(framework: str):
+    return {
+        'posts': [
+            {'title': f'Criando uma aplicação com {framework}', 'date': datetime.now(UTC)}, 
+            {'title': f'Internacionalizando uma app {framework}', 'date': datetime.now(UTC)},
+        ]
+    }   
