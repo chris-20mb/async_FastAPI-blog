83d1b57484751750be27aea335f80f4c2c82acdf
Christopher <christophermachado222@gmail.com>
2024-10-25 12:55:31 -0300

    "adicionado query parameter no endpoint posts"

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from datetime import UTC, datetime
 
 app = FastAPI()
 
+
+fake_db = [
+    {"title": "Criando uma aplicação com Django", "date": datetime.now(UTC), 'published': True},
+    {"title": "Criando uma aplicação com FastAPI", "date": datetime.now(UTC), 'published': True},
+    {"title": "Criando uma aplicação com Flask", "date": datetime.now(UTC), 'published': True},
+    {"title": "Criando uma aplicação com Starllet", "date": datetime.now(UTC), 'published': False},
+]
+
+@app.get("/posts")
+def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
+    return [post for post in fake_db[skip : skip + limit] if post['published'] is published]
+
 @app.get('/posts/{framework}')
-def read_posts(framework: str):
+def read_framework_posts(framework: str):
     return {
         'posts': [
             {'title': f'Criando uma aplicação com {framework}', 'date': datetime.now(UTC)}, 
