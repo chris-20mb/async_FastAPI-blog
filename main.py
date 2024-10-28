7ec732323d237c0262b4d48759a428c6715d47e0
Christopher <christophermachado222@gmail.com>
2024-10-28 15:23:32 -0300

    "updating request body - dynamic parameter"

--- main.py
+++ main.py
@@ -27,10 +27,19 @@ def create_post(post: Post):
 
 
 @app.get("/posts/")
-def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
-    return [post for post in fake_db[skip : skip + limit] if post['published'] is published]
-
+def read_posts(published: bool, limit: int, skip: int = 0):
+    #def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
+    #return [post for post in fake_db[skip : skip + limit] if post['published'] is published]
     #TO DO - alterar aqui para exibir a quantidade correta de posts
+    posts = []
+    for post in fake_db:
+        if len(posts) == limit:
+            break
+        if post["published"] is published:
+            posts.append(post)
+
+    return posts        
+
 
 @app.get('/posts/{framework}')
 def read_framework_posts(framework: str):
