99439a09f289d99256f356617666f32f21f8a0e0
Christopher <christophermachado222@gmail.com>
2024-10-28 12:43:10 -0300

    "incluindo request body"

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def create_post(post: Post):
 def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
     return [post for post in fake_db[skip : skip + limit] if post['published'] is published]
 
+    #TO DO - alterar aqui para exibir a quantidade correta de posts
+
 @app.get('/posts/{framework}')
 def read_framework_posts(framework: str):
     return {
